da4ebb92ed8e2bc39d3d81b23bf07fde0a7da2ab
Eduardo Vizcaino <evigra@hotmail.com>
2020-04-18 00:00:59 -0500

    Add

--- modulos/plazas/modelo.php
+++ modulos/plazas/modelo.php
@@ -15,26 +15,55 @@
 		);
 		*/		
 		var $sys_fields		=array( 
-			"matricula"	    =>array(
-			    "title"             => "Matricula",
+
+			"plaza_id"	    =>array(
+			    "title"             => "plaza",
 				"title_filter"		=> "Matricula",
 			    "type"              => "primary key",
 			    "import"            => "10",			  
 			),
-			"nombre"	    =>array(
-			    "title"             => "Nombre",
-				"title_filter"		=> "Trabajador",
+			"matricula_titular"	    =>array(
+			    "title"             => "Matricula Titular",
+				"title_filter"		=> "Matricula Titular",
 			    "type"              => "input",
 			),
-			"puesto_id"	    =>array(
-			    "title"             => "Puesto ID",
+			"nombre_titular"	    =>array(
+			    "title"             => "Nombre Titular",
+				"title_filter"		=> "Nombre Titular",
 			    "type"              => "input",
 			),
-			"puesto"	    =>array(
-			    "title"             => "Puesto",
-				"title_filter"		=> "Puesto",
+			"matricula"	    =>array(
+			    "title"             => "Matricula",
+			    "title_filter"		=> "Matricula",
+			    "type"              => "input",
+			),
+			"ocupante"	    =>array(
+			    "title"             => "Ocupante",
+				"title_filter"		=> "Ocupante",
 			    "type"              => "input",
 			),			
+			"categoria"	    =>array(
+			    "title"             => "Categoria",
+				"title_filter"		=> "Categoria",
+			    "type"              => "input",
+			),			
+			"jornada"	    =>array(
+			    "title"             => "Jornada",
+			    "type"              => "input",
+			),			
+			"horario"	    =>array(
+			    "title"             => "Horario",
+			    "type"              => "input",
+			),			
+			"ar2"	    =>array(
+			    "title"             => "Departamento",
+			    "type"              => "input",
+			),			
+			"adcripcion2"	    =>array(
+			    "title"             => "Adscripcion",
+			    "type"              => "input",
+			),			
+
 		);				
 		##############################################################################	
 		##  Metodos	
