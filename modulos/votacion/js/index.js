d0227fde966b4f0f0f71533bd0b563cc064beb92
Eduardo Vizcaino <evigra@hotmail.com>
2020-04-19 11:38:58 -0500

    Add

--- modulos/votacion/js/index.js
+++ modulos/votacion/js/index.js
@@ -8,26 +8,23 @@
 			if($(this).val()!="")
 			{
 				$.ajax({
-					type: 'GET',
-					url: '../modulos/plazas/ajax/index.php',
-					contentType:"application/json",
-					data:"&matricula="+$(this).val(),				
+					type:           "GET",
+					url:            "../modulos/plazas/ajax/index.php?time=" + Date.now(),
+					contentType:    "application/json",
+					data:           "&matricula="+$(this).val(),				
 					success: function (response) 
 					{
 						obj = $.parseJSON( response);				
 
                         $("#ocupante").val(obj[0].ocupante);
                         $("#categoria").val(obj[0].categoria);
-                        $("#horario").val(obj[0].horario);
-						
+                        $("#horario").val(obj[0].horario);					
 					}
 				});
 			}	
 		});	
-		
     });
     
     // ###########################################################################
     // ######################### FUNCIONES #######################################
     // ###########################################################################
-    
