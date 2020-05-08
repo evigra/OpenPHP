1dec034bdc5e267f814cc59fcbf8e425407ac6e1
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-08 14:02:23 -0500

    Update

--- nucleo/basededatos.php
+++ nucleo/basededatos.php
@@ -9,10 +9,10 @@
 		public function __SYS_DB()
 		{  
 			return array(
-				"user"		=>"admin_evigra",
-				"pass"		=>"EvG30JiC06",
-				"name"		=>"admin_frame",
-				"host"		=>"solesgps.com",
+				"user"		=>"user",
+				"pass"		=>"password",
+				"name"		=>"database",
+				"host"		=>"localhost",
 				"type"		=>"mysql",
 			);
 		}
