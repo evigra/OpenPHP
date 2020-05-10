5b54199135c92555f73eba757ec30c13699382db
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-10 18:29:46 -0500

    Update

--- modulos/users_apoyamos/js/index.js
+++ modulos/users_apoyamos/js/index.js
@@ -1,6 +1,6 @@
 	$(document).ready(function()
 	{
-        $(".galery").flipping_gallery({
+        $("div.view_report_d2").flipping_gallery({
             enableScroll: true,
             autoplay: 2000
         });	    
