5d28c6db77c8c51ce3973d92886fc3542adc9932
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-10 17:49:19 -0500

    Update

--- modulos/users_apoyamos/index.php
+++ modulos/users_apoyamos/index.php
@@ -6,7 +6,7 @@
 	$objeto->words["system_module"]				=$data["html"];
 
 	$data_js=array(	    
-	    "../sitio_web/js/jquery.flipping_gallery.js",
+	    "../sitio_web/js/jquery.flipping_gallery",
 	    "../" . $objeto->sys_var["module_path"] . "js/index"
 	);
     
