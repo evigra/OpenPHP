6c97deecb96d49c279134ef27aa171a6ed412a10
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-09 23:36:37 -0500

    Update

--- modulos/users_apoyamos/index.php
+++ modulos/users_apoyamos/index.php
@@ -1,37 +1,13 @@
 <?php	
 	$objeto											=new users_apoyamos();
-	
-	# TEMPLATES O PLANTILLAS ELEJIDAS PARA EL MODULO
-	$objeto->words["system_body"]	=	$objeto->__TEMPLATE($objeto->sys_html."system_body");	
-	$objeto->words["system_module"]	=	$objeto->__TEMPLATE($objeto->sys_html."system_module");
-	
-	# CARGA DE ARCHIVOS EXTERNOS JS, CSS
-	$objeto->words["html_head_js"]	=	$objeto->__FILE_JS();
-	#$objeto->words["html_head_css"]	=	$objeto->__FILE_CSS(array("../sitio_web/css/basicItems"));
-		
-	$module_center	="";	
-	$module_left	="";
-    $module_right	="";
-        
-    $module_title	="";
 
-	
-	# TITULO DEL MODULO
-	$module_title                	=	"Crear ";
-
-
-	# CARGANDO VISTA Y CARGANDO CAMPOS A LA VISTA
-	$option										=array();
 	$data										=$objeto->__VIEW_KANBAN($option);		
-	$objeto->words["module_body"]				=$data["html"];
-    	    
-    
-	$objeto->words["module_title"]              ="$module_title Usuarios";
-	
-	$objeto->words["module_left"]               ="";
-	$objeto->words["module_center"]             ="";
-	$objeto->words["module_right"]              ="";
-	
+	$objeto->words["system_module"]				=$data["html"];
+
+
+	$objeto->words["html_head_js"]              =$objeto->__FILE_JS();								# ARCHIVOS JS DEL MODULO
+	$objeto->words["html_head_css"]             =$objeto->__FILE_CSS();
+
 	$objeto->words["html_head_description"]	=	"Todos formamos parte de nuestra del proyecto, y apoyamos a RML en el SNTSS Colima";
 	$objeto->words["html_head_keywords"] 	=	"SNTSS, IMSS";
 
