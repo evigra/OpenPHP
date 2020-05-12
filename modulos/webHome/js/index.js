72096d0b0f842a0faa619dce5e6a670601aaf322
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-12 16:25:06 -0500

    Update

--- modulos/webHome/js/index.js
+++ modulos/webHome/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function()
 {       
-    /*  
+    ///*  
     $(".view_report_d2").flipping_gallery({
         enableScroll: true,
         autoplay: 2000
     });
-    */	
+    //*/	
 });
