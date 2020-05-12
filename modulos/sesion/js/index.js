821d67dc685e5e8e209c41b777d881583bc500bf
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-12 18:56:56 -0500

    Update

--- modulos/sesion/js/index.js
+++ modulos/sesion/js/index.js
@@ -1,7 +1,6 @@
 	$(document).ready(function()
-	{
-	   
-	    $("#contenedor").parents( "td" ).css( "background","#373737"); 
+	{	   
+	    //$("#contenedor").parents( "td" ).css( "background","#373737"); 
 	    $("#sys_section_sesion").val("create");
 		$("#sys_action_sesion").val("__SAVE");
     });
