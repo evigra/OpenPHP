487f690228614aa79845e98fa30a3d962aff3f35
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-11 22:53:51 -0500

    Update

--- modulos/webHome/modelo.php
+++ modulos/webHome/modelo.php
@@ -12,9 +12,11 @@
 		public function __CONSTRUCT($option=null)
 		{			    
             unset($this->sys_fields["usergroup_ids"]);
+            unset($this->sys_fields["sesion_start"]);
+            unset($this->sys_fields["company_id"]);
 
+            $this->__PRINT_R($this->sys_fields);
 			parent::__CONSTRUCT($option);
-		}		
-		
+		}				
 	}
 ?>
