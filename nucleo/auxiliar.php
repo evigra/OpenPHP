7555ae65b9085a005a509bc001e2936ac40c0128
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-12 13:44:48 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -3007,7 +3007,6 @@
 						elseif($option["type_view"]=="galery")
 						{
 							$return["report"]="
-							        LALO
 								    <div style=\"height:100%;\"  class=\"view_report_d2\">
 						                $view_body									            
 							        </div>
