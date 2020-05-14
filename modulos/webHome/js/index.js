14cc59cad122bde3caf56def5b6ac9aab0e89bfc
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-14 00:04:45 -0500

    Update

--- modulos/webHome/js/index.js
+++ modulos/webHome/js/index.js
@@ -1,19 +1,24 @@
 $(document).ready(function()
-{       
-    /*  
-    $(".view_report_d2").flipping_gallery({
-        enableScroll: true,
-        autoplay: 2000
-    });
-    //*/	
-    
-
+{  
+    $("img.galery")
+        .before("<div>" )     
+        .after("<div>");
     $(".galery").first().show("slow", siguiente);    
+    alert($(".galery").length -1);
 });
 
 function siguiente()
 {
-    $(this).next(".galery").hide("slow");
-    $(this).next(".galery").show("slow", siguiente);
-    
+    alert(parseInt($(".galery").index(this)) + "==" + parseInt($(".galery").length) -1 );
+
+    if(parseInt($(".galery").index(this))== parseInt($(".galery").length) -1 )
+    {
+        $(this).first().hide("slow");
+        $(this).first().show("slow", siguiente);        
+    }
+    else
+    {
+        $(this).next(".galery").hide("slow");
+        $(this).next(".galery").show("slow", siguiente);
+    }    
 }
