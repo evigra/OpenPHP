6910c4f5026ac7098271ee5b582365c31b08bdf2
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-14 00:44:28 -0500

    Update

--- modulos/webHome/js/index.js
+++ modulos/webHome/js/index.js
@@ -11,7 +11,7 @@ $(document).ready(function()
         $( ".galery" ).stop();
     })
     .mouseout(function() {
-        $(".galery").hide("slow");
+        $(this).hide("slow");
         $(this).next(".galery").show("slow", siguiente);
     });
 
