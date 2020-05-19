5d528b39419239800efa27dbdb33aae50a992912
Eduardo Vizcaino <evigra@hotmail.com>
2020-05-19 16:58:16 -0500

    Update

--- modulos/user_group/modelo.php
+++ modulos/user_group/modelo.php
@@ -42,9 +42,11 @@
 		}
    		public function __SAVE($datas=NULL,$option=NULL)
     	{
+    	    if(!isset($_SESSION["company"]["id"]))     $_SESSION["company"]["id"]=1
+    	
     		if(is_array($datas))
-	    	    $datas["company_id"]    	=@$_SESSION["company"]["id"];
-    		return parent::__SAVE($datas,$option);
+	    	    $datas["company_id"]    	=@$_SESSION["company"]["id"];            
+    		return parent::__SAVE($datas,$option);    		
 		}		
 	}
 ?>
