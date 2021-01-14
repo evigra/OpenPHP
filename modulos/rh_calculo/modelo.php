5e745834270f996a71f7a75010d1c9a5de812a94
Eduardo Vizcaino <evigra@hotmail.com>
2021-01-14 16:12:56 -0600

    Update

--- modulos/rh_calculo/modelo.php
+++ modulos/rh_calculo/modelo.php
@@ -471,8 +471,6 @@
 			    "default"           => "",
 			    "value"             => "",			    
 			),				
-				
-
 			/*
 			"conceptos_ids"	    =>array(
 			    "title"             => "Horario",
@@ -538,7 +536,6 @@
     		## GUARDAR USUARIO
     		#$datas["total"]		=count(explode(",",$datas["dias"]));
 			#$datas["registro"]=$this->sys_date;
-			/*
 			if($datas["estatus"]=="APROVADO")
 			{
 				$datas["autorizo"]		=$_SESSION["user"]["nombre"];
@@ -552,7 +549,7 @@
 				$datas["elaboro"]		=$_SESSION["user"]["nombre"];
 				$datas["m_elaboro"]		=$_SESSION["user"]["matricula"];				
 			}
-			*/
+			
 			#$datas["cptos_fijos"]		=count($datas["fijos_ids"]);		
 			#$datas["cpto_1vez"]			=count($datas["conceptos_ids"]);		
 			#$datas["hist_acum"]			=count($datas["historicos_ids"]);		
@@ -563,6 +560,8 @@
 			#$this->__PRINT_R($datas);
 			##########################33    		
     	    $save	= parent::__SAVE($datas,$option);
+
+    	    
 			if($this->sys_private["section"]=="create")    		
 			{
 				$option_print=array(
@@ -571,9 +570,25 @@
 					"module"	=>$this->sys_object,
 				);			
 				$this->PDF_PRINT($option_print);
-			}  	        	    
+			}
+
+
+
+    	    
+    	    
+    	    
     	    return $save;
     	    
+		/*
+		public function PDF_PRINT($option=null)
+		{						
+			if(!is_array($option))
+				$option=array(
+					"id"		=>"$option",
+					"section"	=>"write",
+					"module"	=>$this->sys_object,
+				);
+		*/    	    
     	    
     	    
 		}
